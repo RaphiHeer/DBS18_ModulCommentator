5fac7ef0fb523d211a9f3aebdbf1029d8fb38844
Luca Rava <luca.rava@stud.hslu.ch>
2018-05-23 22:31:15 +0200

    Implementation Abfrage auf DB

---
+++ RavenDB_TestQueries/RavenDB/DB_Operation.cs
@@ -0,0 +1,131 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace RavenDB
+{
+    class DB_Operation
+    {
+        IDocumentStore store;
+
+        public DB_Operation()
+        {
+            store = DocumentStoreHolder.Store;
+        }
+
+        public void addStudent(Student student)
+        {
+            addStudent(student.Id, student.username, student.email, student.nachname, student.vorname);
+        }
+
+        public void addStudent(string id, string username, string email, string nachname, string vorname)
+        {
+            using (IDocumentSession session = store.OpenSession())
+            {
+                Student newStudent = new Student(id, username, email, nachname, vorname);
+                session.Store(newStudent);
+                session.SaveChanges();
+            }
+        }
+
+
+        //Beispiel von Präsentator um student zu holen
+        public Student GetStuden(string vorname)
+        {
+            using (IDocumentSession session = store.OpenSession())
+            {
+                var student = session.Query<Student>().Where<Student>(x => x.nachname == vorname).First();
+                return student;
+            }
+        }
+
+        public void addDozent(Dozenten dozent)
+        {
+            addDozent(dozent.Id, dozent.vorname, dozent.nachname, dozent.kuerzel, dozent.email);
+        }
+
+        public void addDozent(string id, string kuerzel, string email, string nachname, string vorname)
+        {
+            using (IDocumentSession session = store.OpenSession())
+            {
+                Dozenten newDozent = new Dozenten(id, kuerzel, email, nachname, vorname);
+                session.Store(newDozent);
+                session.SaveChanges();
+            }
+        }
+
+        public void addModul(Modul modul)
+        {
+            addModul(modul.Id, modul.bezeichnung, modul.kuerzel, modul.verantwortlicher);
+        }
+
+        public void addModul(string id, string bezeichnung, string kuerzel, string verantwortlicher)
+        {
+            using (IDocumentSession session = store.OpenSession())
+            {
+                Modul newModul = new Modul(id, bezeichnung, kuerzel, verantwortlicher);
+                session.Store(newModul);
+                session.SaveChanges();
+            }
+        }
+
+        public void addComment(Kommentar comment)
+        {
+            addComment(comment.Id, comment.text, comment.geschrieben_von, comment.modul, comment.Dozenten);
+        }
+
+        public void addComment(string id, string text, string geschrieben_von, string modul, List<string> dozenten)
+        {
+            using (IDocumentSession session = store.OpenSession())
+            {
+                Kommentar newComment = new Kommentar(id, text, geschrieben_von, modul, dozenten);
+                session.Store(newComment);
+                session.SaveChanges();
+            }
+        }
+
+        public Dozenten getDozent(string id)
+        {
+            Dozenten dozent = null;
+
+            using (IDocumentSession session = store.OpenSession())
+            {
+                dozent = session.Load<Dozenten>(id);
+            }
+
+            return dozent;
+        }
+
+        public Student getStudent(string id)
+        {
+            Student student = null;
+
+            using (IDocumentSession session = store.OpenSession())
+            {
+                student = session.Load<Student>(id);
+            }
+
+            return student;
+        }
+
+        public Modul getModul(string id)
+        {
+            Modul modul = null;
+
+            using (IDocumentSession session = store.OpenSession())
+            {
+                modul = session.Load<Modul>(id);
+            }
+
+            return modul;
+        }
+    }
+
+    
+}
---
+++ RavenDB_TestQueries/RavenDB/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RavenDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DB_Operation operation = new DB_Operation();
+
+            Student student1 = new Student("Student|", "stu1", "stu1@stud.hslu.ch", "Test", "Student1");
+            Student student2 = new Student("Student|", "stu2", "stu2@stud.hslu.ch", "Test", "Student2");
+
+            operation.addStudent(student1);
+            operation.addStudent(student2);
+
+            Dozenten dozent1 = new Dozenten("Dozent|", "doz1", "doz1@stud.hslu.ch", "Test", "Dozent1");
+            Dozenten dozent2 = new Dozenten("Dozent|", "doz2", "doz2@stud.hslu.ch", "Test", "Dozent2");
+
+            operation.addDozent(dozent1);
+            operation.addDozent(dozent2);
+
+            Modul modul1 = new Modul("Modul|", "TestModul1", "TM1", operation.getDozent("Dozent/19").Id);
+            Modul modul2 = new Modul("Modul|", "TestModul2", "TM2", operation.getDozent("Dozent/20").Id);
+
+            operation.addModul(modul1);
+            operation.addModul(modul2);
+            
+            List<string> dozenten = new List<string>();
+            dozenten.Add(operation.getDozent("Dozent/19").Id);
+            dozenten.Add(operation.getDozent("Dozent/20").Id);
+
+            Kommentar comment = new Kommentar("Kommentar|", "Test1", operation.getStudent("Student/19").Id, operation.getModul("Modul/19").Id, dozenten);            
+
+            operation.addComment(comment);
+
+            operation
+        }
+    }
+}
